d5eabac0f6f2dbef01de53d844e6734959e09650
yakutov <miti_95@mail.ru>
2015-10-06 16:27:33 +0300

    Commit

--- src/util/math/GeometryUtils.java
+++ src/util/math/GeometryUtils.java
@@ -1,6 +1,7 @@
 package util.math;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public abstract class GeometryUtils {
 
@@ -124,12 +125,14 @@ public abstract class GeometryUtils {
         for (int i = 1; i < n; i++) {
             a[i] = a[i].subtract(start);
         }
-        Arrays.sort(a, 1, a.length, (o1, o2) -> {
-            double m = o1.mulv(o2);
-            if (signum(m) != 0) {
-                return -signum(m);
+        Arrays.sort(a, 1, a.length, new Comparator<Point2DDouble>() {
+            public int compare(Point2DDouble o1, Point2DDouble o2) {
+                double m = o1.mulv(o2);
+                if (signum(m) != 0) {
+                    return -signum(m);
+                }
+                return signum(o1.length2() - o2.length2());
             }
-            return signum(o1.length2() - o2.length2());
         });
         for (int i = 1; i < n; i++) {
             a[i] = a[i].add(start);
